1f440b5c55631df01c6db1e8095571ec9dd05058
David Granado <davidjgranado@gmail.com>
2012-02-22 13:46:20 -0600

    Update src/php/views/main.php

--- src/php/views/main.php
+++ src/php/views/main.php
@@ -18,18 +18,5 @@
 		<div id='content'>
 		</div>
 		<script data-main="src/js/main.js" src="libs/js/require.js"></script>
-		<script type="text/javascript">
-			var _gaq = _gaq || [];
-			_gaq.push(['_setAccount', 'UA-7940406-1']);
-			_gaq.push(['_trackPageview']);
-							
-			(function() {
-				var ga = document.createElement('script');
-				ga.type = 'text/javascript';
-				ga.async = true;
-				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();
-		</script>
 	</body>
 </html>
